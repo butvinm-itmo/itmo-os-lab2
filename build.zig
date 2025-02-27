bfec6a6354f3280b610371042d692bb704f26fbd
Mihail Butvin <butvin.mihail@yandex.ru>
2025-02-27 05:20:31 +0300

    Refactor build

--- build.zig
+++ build.zig
@@ -1,39 +1,38 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
+    const shell_exe = b.addExecutable(.{
         .name = "shell",
         .root_source_file = b.path("src/shell/shell.zig"),
         .target = target,
         .optimize = optimize,
     });
 
-    b.installArtifact(exe);
+    b.installArtifact(shell_exe);
 
-    const run_shell = b.addRunArtifact(exe);
-
-    run_shell.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_shell.addArgs(args);
-    }
-
-    const run_shell_step = b.step("run_shell", "Run the shell");
+    const run_shell = b.addRunArtifact(shell_exe);
+    if (b.args) |args| run_shell.addArgs(args);
+    const run_shell_step = b.step("run-shell", "Run the shell");
     run_shell_step.dependOn(&run_shell.step);
-
-    const unit_tests = b.addTest(.{
+    
+    const test_step = b.step("test", "Run unit tests");
+    
+    const shell_tests = b.addTest(.{
         .root_source_file = b.path("src/shell/tests.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    const run_unit_tests = b.addRunArtifact(unit_tests);
-
-    const test_step = b.step("test_shell", "Run unit tests");
-    test_step.dependOn(&run_unit_tests.step);
+    const run_shell_tests = b.addRunArtifact(shell_tests);
+    test_step.dependOn(&run_shell_tests.step);
+    
+    const linreg_tests = b.addTest(.{
+        .root_source_file = b.path("src/algorithms/linreg.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_linreg_tests = b.addRunArtifact(linreg_tests);
+    test_step.dependOn(&run_linreg_tests.step);
 }
