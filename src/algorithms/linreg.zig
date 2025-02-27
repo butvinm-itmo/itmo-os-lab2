cdd1adeee2a2b8d12c766633364b8e381bbb8bef
Mihail Butvin <butvin.mihail@yandex.ru>
2025-02-27 11:47:35 +0300

    Implement linreg and search_name

---
+++ src/algorithms/linreg.zig
@@ -0,0 +1,43 @@
+const std = @import("std");
+
+pub fn LinReg(n: comptime_int) type {
+    return struct {
+        const Self = @This();
+
+        m: f64,
+        b: f64,
+
+        pub fn fit(xs: @Vector(n, f64), ys: @Vector(n, f64)) Self {
+            const sum_x = @reduce(.Add, xs);
+            const sum_y = @reduce(.Add, ys);
+            const sum_xx = @reduce(.Add, xs * xs);
+            const sum_xy = @reduce(.Add, xs * ys);
+            const m = (n * sum_xy - sum_x * sum_y) / (n * sum_xx - sum_x * sum_x);
+            const b = (sum_y - m * sum_x) / n;
+            return .{ .m = m, .b = b };
+        }
+
+        pub fn predict(self: Self, x: f64) f64 {
+            return self.m * x + self.b;
+        }
+    };
+}
+
+test {
+    const n = 5;
+    const model = LinReg(n).fit(
+        @Vector(n, f64){ 1, 2, 3, 4, 5 },
+        @Vector(n, f64){ 2, 3, 5, 7, 8 },
+    );
+
+    try std.testing.expectApproxEqRel(0.2, model.predict(0), 1e-10);
+    try std.testing.expectApproxEqRel(1.8, model.predict(1), 1e-10);
+    try std.testing.expectApproxEqRel(3.4, model.predict(2), 1e-10);
+    try std.testing.expectApproxEqRel(5, model.predict(3), 1e-10);
+    try std.testing.expectApproxEqRel(6.6, model.predict(4), 1e-10);
+    try std.testing.expectApproxEqRel(8.2, model.predict(5), 1e-10);
+    try std.testing.expectApproxEqRel(9.8, model.predict(6), 1e-10);
+    try std.testing.expectApproxEqRel(11.4, model.predict(7), 1e-10);
+    try std.testing.expectApproxEqRel(13, model.predict(8), 1e-10);
+    try std.testing.expectApproxEqRel(14.6, model.predict(9), 1e-10);
+}
---
+++ src/algorithms/search_name.zig
@@ -0,0 +1,76 @@
+const std = @import("std");
+const fs = std.fs;
+
+/// Find files containing `name` in the filename.
+/// Search in the root path if specified, otherwise search in the current working directory.
+pub fn search_name(alloc: std.mem.Allocator, name: []const u8, root: ?[]const u8) !SearchIterator {
+    const root_dir = try open_root_dir(alloc, root);
+    return SearchIterator.init(alloc, name, root_dir);
+}
+
+fn open_root_dir(alloc: std.mem.Allocator, root: ?[]const u8) !fs.Dir {
+    if (root) |root_path| {
+        const real_root_path = try fs.realpathAlloc(alloc, root_path);
+        defer alloc.free(real_root_path);
+        return fs.openDirAbsolute(real_root_path, .{ .iterate = true, .access_sub_paths = true });
+    } else {
+        return fs.cwd().openDir(".", .{ .iterate = true, .access_sub_paths = true });
+    }
+}
+
+pub const SearchIterator = struct {
+    target: []const u8,
+    root_dir: fs.Dir,
+    walker: fs.Dir.Walker,
+
+    /// Initialize walker.
+    /// Walker owns basedir and will closed it on deinit.
+    /// If initialization fails, root_dir will be closed.
+    pub fn init(alloc: std.mem.Allocator, target: []const u8, root_dir: fs.Dir) !SearchIterator {
+        const walker = try root_dir.walk(alloc);
+        errdefer root_dir.close();
+        return .{ .target = target, .root_dir = root_dir, .walker = walker };
+    }
+
+    pub fn deinit(self: *SearchIterator) void {
+        self.walker.deinit();
+        self.root_dir.close();
+    }
+
+    pub fn next(self: *SearchIterator) !?fs.Dir.Walker.Entry {
+        while (try self.walker.next()) |file| {
+            if (std.mem.indexOf(u8, file.basename, self.target)) |_| {
+                return file;
+            }
+        }
+        return null;
+    }
+};
+
+test "searh in cwd" {
+    var search = try search_name(std.testing.allocator, "build.zig", null);
+    defer search.deinit();
+
+    var file = try search.next();
+    try std.testing.expectEqualStrings("build.zig", file.?.basename);
+
+    file = try search.next();
+    try std.testing.expectEqualStrings("build.zig.zon", file.?.basename);
+    
+    file = try search.next();
+    try std.testing.expectEqual(null, file);
+}
+
+test "searh in provided root path" {
+    var search = try search_name(std.testing.allocator, ".zig", "src/algorithms");
+    defer search.deinit();
+
+    var file = try search.next();
+    try std.testing.expectEqualStrings("linreg.zig", file.?.basename);
+
+    file = try search.next();
+    try std.testing.expectEqualStrings("search_name.zig", file.?.basename);
+    
+    file = try search.next();
+    try std.testing.expectEqual(null, file);
+}
